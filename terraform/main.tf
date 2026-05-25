485afef1562ade6a892966c70be81f56ba43e609
marksmith388 <markdsmith35@gmail.com>
2026-05-25 12:47:17 +0100

    Added 2 PROD VMs and Dev Machine

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,14 @@ provider "aws" {
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
+  enable_dns_hostnames = true
   tags = {
     Name = "Linux Demo VPC"
   }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
 }
 
 resource "aws_subnet" "my_subnet" {
@@ -30,12 +35,70 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "app_server" {
+data "aws_ami" "debian-11" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["debian-11-amd64-*"]
+  }
+
+  owners = ["136693071363"] # Canonical
+}
+
+resource "aws_instance" "app_server-1" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.medium"
   subnet_id     = aws_subnet.my_subnet.id
 
   tags = {
-    Name = "learn-terraform"
+    Name = "prod-vm-1"
   }
+}
+
+resource "aws_instance" "app_server-2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.medium"
+  subnet_id     = aws_subnet.my_subnet.id
+
+  tags = {
+    Name = "prod-vm-2"
+  }
+}
+
+resource "aws_instance" "app_server-3" {
+  ami           = data.aws_ami.debian-11.id
+  instance_type = "t3.medium"
+  subnet_id     = aws_subnet.my_subnet.id
+
+  tags = {
+    Name = "prod-vm-3"
+  }
+}
+
+resource "aws_instance" "dev-machine" {
+  ami           = data.aws_ami.debian-11.id
+  instance_type = "t3.medium"
+  subnet_id     = aws_subnet.my_subnet.id
+
+  tags = {
+    Name = "dev-machine"
+  }
+}
+
+#Creating elastic IP
+
+resource "aws_eip" "lb" {
+  domain = "vpc"
+
+  tags = {
+    Name = "fluffy-eip"
+  }
+}
+
+#Attaching Elatic IP to Instance 
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.dev-machine.id
+  allocation_id = aws_eip.lb.id
 }
