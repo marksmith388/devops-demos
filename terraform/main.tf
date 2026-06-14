cc9ac0057b5a6259848ad5b42b408bf185f7279b
marksmith388 <markdsmith35@gmail.com>
2026-06-14 14:11:26 +0100

    Added Ansible Control Node and built management Subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,15 @@ resource "aws_subnet" "my_subnet" {
   }
 }
 
+resource "aws_network_interface" "primary_enis" {
+  count = 3
+  subnet_id       = aws_subnet.my_subnet.id
+  private_ips     = ["172.16.10.${250 + count.index}"]
+
+  # Attach your security group directly to the interface
+  security_groups = [aws_security_group.allow_ssh.id]
+}
+
 resource "aws_subnet" "my_subnet2" {
   vpc_id            = aws_vpc.my_vpc.id
   availability_zone = "eu-west-1a"
@@ -34,13 +43,23 @@ resource "aws_subnet" "my_subnet2" {
   }
 }
 
-resource "aws_network_interface" "secondary_ip" {
+resource "aws_network_interface" "secondary_enis" {
+  count = 3
   subnet_id       = aws_subnet.my_subnet2.id
-  private_ips     = ["172.16.11.250"]
+  private_ips     = ["172.16.11.${250 + count.index}"]
 
-  attachment {
-    instance     = aws_instance.app_server-1.id
-    device_index = 1
+  # Attach your security group directly to the interface
+  security_groups = [aws_security_group.allow_ssh.id]
+}
+
+resource "aws_subnet" "management_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  availability_zone       = "eu-west-1a"
+  cidr_block              = "172.16.20.0/24" # A fresh, unconflicted subnet range
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Management Public Subnet"
   }
 }
 
@@ -69,12 +88,11 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# Associate the Route Table with the Subnet
-resource "aws_route_table_association" "public_assoc2" {
-  subnet_id      = aws_subnet.my_subnet2.id
+# Associate ONLY this new subnet to your Internet Gateway Route Table
+resource "aws_route_table_association" "mgmt_assoc" {
+  subnet_id      = aws_subnet.management_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
-
 
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
@@ -126,26 +144,37 @@ data "aws_ami" "debian-11" {
   owners = ["136693071363"] # Canonical
 }
 
-resource "aws_instance" "app_server-1" {
+resource "aws_instance" "app_server" {
+  count = 3
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.medium"
-  subnet_id     = aws_subnet.my_subnet.id
 
   # Reference the key pair name from the resource above
   key_name      = aws_key_pair.deployer.key_name
 
-  # Attach the security group here
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  # Primary Interface (Subnet 1 - Auto-allocated by AWS)
+  network_interface {
+    network_interface_id = aws_network_interface.primary_enis[count.index].id
+    device_index         = 0
+  }
+
+  # Secondary Interface (Subnet 2 - Your Static Management IPs)
+  network_interface {
+    network_interface_id = aws_network_interface.secondary_enis[count.index].id
+    device_index         = 1
+  }
 
   tags = {
-    Name = "prod-vm-1"
+    Name = "prod-vm-${count.index + 1}"
   }
+
+  user_data = file("../scripts/add-ssh-key.yaml")
 }
 
-resource "aws_instance" "app_server-2" {
+resource "aws_instance" "dev_ops_machine" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.medium"
-  subnet_id     = aws_subnet.my_subnet.id
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.management_subnet.id
 
   # Reference the key pair name from the resource above
   key_name      = aws_key_pair.deployer.key_name
@@ -153,39 +182,26 @@ resource "aws_instance" "app_server-2" {
   # Attach the security group here
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
+  associate_public_ip_address = true
+
   tags = {
-    Name = "prod-vm-2"
+    Name = "dev_ops_machine"
+  }
+
+  user_data = file("../scripts/install-ansible.yaml")
+
+}
+
+
+
+output app_server_ip{
+  description = "Private IPs for the VMs"
+  value = { 
+    for server in aws_instance.app_server : server.tags["Name"] => server.public_dns
   }
 }
 
-resource "aws_instance" "app_server-3" {
-  ami           = data.aws_ami.debian-11.id
-  instance_type = "t3.medium"
-  subnet_id     = aws_subnet.my_subnet.id
-
-  # Reference the key pair name from the resource above
-  key_name      = aws_key_pair.deployer.key_name
-
-  # Attach the security group here
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-
-  tags = {
-    Name = "prod-vm-3"
-  }
-}
-
-resource "aws_instance" "dev-machine" {
-  ami           = data.aws_ami.debian-11.id
-  instance_type = "t3.medium"
-  subnet_id     = aws_subnet.my_subnet.id
-
-  # Reference the key pair name from the resource above
-  key_name      = aws_key_pair.deployer.key_name
-
-  # Attach the security group here
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-
-  tags = {
-    Name = "dev-machine"
-  }
+output dev_ops_machine_dns_name{
+  description  = "DNS name for dev_ops_machine"
+  value = aws_instance.dev_ops_machine.public_dns
 }
